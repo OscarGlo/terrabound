e6b85995ffa57fd93fcbe6512ac0450d852366cb
anunknownguy <warion.hugo@gmail.com>
2020-06-13 12:04:14 +0200

    update World
    for loops

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -33,23 +33,24 @@ func load():
 
 func loadChunks():
 	
-	var loadRadius = Math.Floor(loadSize / 2)
+	var loadRadius = VectorUtil.Floor(load_size / 2)
 
 	var chunks = []
 	var playerChunk = VectorUtil.Floor((player.Position + player.Size / 2) / (chunkSize * TileSize))
 
 	#Console.WriteLine(playerChunk.X + " |" + playerChunk.Y);
 
-	for (var x = playerChunk.X - loadRadius.X; x <= playerChunk.X + loadRadius.X; x++)
-		for (var y = playerChunk.Y - loadRadius.Y; y <= playerChunk.Y + loadRadius.Y; y++)
-    		chunks.Add(new Vector2(x, y)
-    		#Console.WriteLine(x + " |" + y)
+	for x in range(playerChunk.X - loadRadius.X, playerChunk.X + loadRadius.X):
+		for y in range(playerChunk.Y - loadRadius.Y, playerChunk.Y + loadRadius.Y):
+			chunks.push_back(Vector2(x, y))
+			#Console.WriteLine(x + " |" + y)
 
 			FrontLayer.UpdateChunks(chunks.ToArray(), ChunkLayer, 0);
-            //backLayer.UpdateChunks(chunks.ToArray(), chunkLayer, 1);
+            #backLayer.UpdateChunks(chunks.ToArray(), chunkLayer, 1);
 	pass
 
 func saveTiles(chunckPos: Vector2 , tiles: Dictionary): #*<Vector2, Tile>*#
+	ChunkLayer.SaveTiles(chunckPos, tiles);
 	pass
 
 func update():
